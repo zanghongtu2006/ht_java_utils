267eeb3f9438654e72585532784511b339f70c7e
hongtu_zang <zanghongtu2006@gmail.com>
2015-07-10 14:38:09 +0800

    add function comment and usage

--- src/main/java/com/chinacloudly/htjutils/md5/MD5Util.java
+++ src/main/java/com/chinacloudly/htjutils/md5/MD5Util.java
@@ -3,7 +3,10 @@ package com.chinacloudly.htjutils.md5;
 import java.security.MessageDigest;
 
 public class MD5Util {
-
+	
+	/*
+	 * md5 a string
+	 */
 	public final static String MD5(String s) {
 		char hexDigits[]={'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
 		try {
@@ -26,6 +29,10 @@ public class MD5Util {
 		}
 	}
 	
+	/*
+	 * md5 a string with salt
+	 * md5(s + salt)
+	 */
 	public final static String saltMD5(String s, String salt) {
 		return MD5(s + salt);
 	}
