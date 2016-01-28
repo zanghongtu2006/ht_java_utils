6382ae93d194b124cc1a2ca9425c4fa29d8ff528
hongtu_zang <zanghongtu2006@gmail.com>
2016-01-28 16:05:24 +0800

    add function getServerIdByID

--- src/com/chinacloudly/utils/id/generator/IDGenerator.java
+++ src/com/chinacloudly/utils/id/generator/IDGenerator.java
@@ -4,19 +4,26 @@ package com.chinacloudly.utils.id.generator;
 public class IDGenerator {
 	
 	private static long ID_FLAG = 1260000000000L;
-	private static long SERVER_ID = 1L;
+	private static int SERVER_ID = 1;
 	
 	/**
 	 * Generate auto added id with current time, can distribute on AT MOST 128 machines
 	 * @return long id
 	 * @throws Exception
 	 */
-	public final static long generateId() throws Exception {
+	
+	/**
+	 * Generate auto added id with current time, can distribute on AT MOST 128 machines
+	 * @param serverID every server allocate its uniq serverID, 0 < serverID < 127
+	 * @return long id
+	 * @throws Exception
+	 */
+	public final static long generateId(int serverID) throws Exception {
 		if (SERVER_ID <= 0)
 			throw new Exception("server id is error, please check!");
 
 		long infoid = System.currentTimeMillis() - ID_FLAG;
-		infoid = (infoid << 7) | SERVER_ID;
+		infoid = (infoid << 7) | serverID;
 		Thread.sleep(1);
 		return infoid;
 	}
@@ -29,5 +36,15 @@ public class IDGenerator {
 	public final static long getTimeByID(long id) {
 		long timeMilis = (id >> 7) + ID_FLAG;
 		return timeMilis;
+	}
+	
+	/**
+	 * Get serverID by id
+	 * @param id
+	 * @return serverID
+	 */
+	public final static int getServerIdByID(long id) {
+		byte serverId = (byte)(id & 0x7F);
+		return (int)serverId;
 	}
 }
--- test/com/chinacloudly/utils/id/generator/TestIDGenerator.java
+++ test/com/chinacloudly/utils/id/generator/TestIDGenerator.java
@@ -10,14 +10,21 @@ public class TestIDGenerator {
 
 	@Test
 	public void testIDGenerator() throws Exception {
-		System.out.println(IDGenerator.generateId());
+		System.out.println(IDGenerator.generateId(1));
 	}
 	
 	@Test
 	public void testGetTimeById() throws Exception {
-		long id = IDGenerator.generateId();
+		long id = IDGenerator.generateId(1);
 		long time = IDGenerator.getTimeByID(id);
 		System.out.println(time);
 		System.out.println(new Date(time));
+	}
+	
+	@Test
+	public void testGetServerIdByID() throws Exception {
+		long id = IDGenerator.generateId(12);
+		int serverID = IDGenerator.getServerIdByID(id);
+		System.out.println(serverID);
 	}
 }
