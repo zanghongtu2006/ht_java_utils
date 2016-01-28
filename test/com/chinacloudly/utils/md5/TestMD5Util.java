5c54efec2abaf7dda1ff1e26ffff5af6ceefa0a6
hongtu_zang <zanghongtu2006@gmail.com>
2016-01-28 15:18:04 +0800

    add unit test

---
+++ test/com/chinacloudly/utils/md5/TestMD5Util.java
@@ -0,0 +1,18 @@
+package com.chinacloudly.utils.md5;
+
+import org.junit.Test;
+
+public class TestMD5Util {
+	
+	@Test
+	public void TestMD5() {
+		String source = "hello world!";
+		System.out.println(MD5Util.MD5(source));
+	}
+	
+	@Test
+	public void saltMD5() {
+		String source = "hello world!";
+		System.out.println(MD5Util.saltMD5(source, "1234"));
+	}
+}
