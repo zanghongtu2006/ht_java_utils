bcb75a68a659f2725fc1a4b0c9bbbe51d784d318
hongtu_zang <zanghongtu2006@gmail.com>
2016-04-27 10:43:55 +0800

    add string utils. some functions to deal a String

---
+++ src/com/chinacloudly/utils/string/StringUtils.java
@@ -0,0 +1,9 @@
+package com.chinacloudly.utils.string;
+
+public class StringUtils {
+
+	public static String notNullString(final String valueString) {
+		return (null == valueString) ? "" : valueString;
+	}
+	
+}
