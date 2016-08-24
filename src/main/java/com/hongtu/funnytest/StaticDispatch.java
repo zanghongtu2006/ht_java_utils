12a54f3b91dfba40bcc2954ddd9727592de2c243
hongtu_zang <zanghongtu2006@gmail.com>
2016-08-24 18:37:53 +0800

    add 2 funny tests

---
+++ src/main/java/com/hongtu/funnytest/StaticDispatch.java
@@ -0,0 +1,35 @@
+package com.hongtu.funnytest;
+
+/**
+ * Created by hongtu_zang on 2016/8/24.
+ */
+public class StaticDispatch {
+    static abstract class Human {
+    }
+
+    static class Man extends Human {
+    }
+
+    static class Woman extends Human {
+    }
+
+    public void sayHello(Human guy) {
+        System.out.println("hello guy");
+    }
+
+    public void sayHello(Man guy) {
+        System.out.println("hello guy");
+    }
+
+    public void sayHello(Woman guy) {
+        System.out.println("hello guy");
+    }
+
+    public static void main(String[] args) {
+        Human man = new Man();
+        Human woman = new Woman();
+        StaticDispatch sd = new StaticDispatch();
+        sd.sayHello(man);
+        sd.sayHello(woman);
+    }
+}
