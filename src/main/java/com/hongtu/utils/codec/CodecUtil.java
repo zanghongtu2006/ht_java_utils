0f533689b886c2d4f63769dd8ff085af746c68de
hongtu_zang <zanghongtu2006@gmail.com>
2016-08-29 17:39:17 +0800

    新增编解码工具类

---
+++ src/main/java/com/hongtu/utils/codec/CodecUtil.java
@@ -0,0 +1,38 @@
+package com.hongtu.utils.codec;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+
+/**
+ * 编码与解码操作工具类
+ * Created by hongtu_zang on 2016/8/29.
+ */
+public class CodecUtil {
+
+    /**
+     * 将 URL 编码
+     */
+    public static String encodeURL(String source) {
+        String target;
+        try {
+            target = URLEncoder.encode(source, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        return target;
+    }
+
+    /**
+     * 将 URL 解码
+     */
+    public static String decodeURL(String source) {
+        String target;
+        try {
+            target = URLDecoder.decode(source, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            throw new RuntimeException(e);
+        }
+        return target;
+    }
+}
